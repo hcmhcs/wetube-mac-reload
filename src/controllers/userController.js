729bd1fac975d3a7e389d07019e3773628f8e993
hcmhcs <hcmhcs0@ajou.ac.kr>
2022-07-17 01:21:55 +0900

    로그인시스템까지 완료

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,63 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
 };
 export const postJoin = async (req, res) => {
-  const { name, email, username, password, location } = req.body;
-  const usernameExists = await User.exists({ username });
-  if (usernameExists) {
-    return res.render("join", {
-      pageTitle: "Join",
-      errorMessage: "This username is already taken.",
+  const { name, email, username, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  res.redirect("/login");
+
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken",
+    });
+  }
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("/join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Login" });
+};
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {});
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("l ogin", {
+      pageTitle: "Login",
+      errorMessage: "Wrong password!",
+    });
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
+};
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
